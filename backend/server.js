60574818c4769fed7b0ca6ed8678761ee480fa98
Coding City <codingcity237@gmail.com>
2026-04-03 00:01:08 +0100

    ajout du lien render

--- backend/server.js
+++ backend/server.js
@@ -17,8 +17,7 @@ app.use(express.json()); // Permet de traiter le corps des requetes en JSON (pou
 // app.use(express.urlencoded({ extended: true }));
 app.use(
 	cors({
-		origin: process.env.FRONTEND_URL || "http://localhost:5173",
-		credentials: true,
+		origin: "*",
 	}),
 ); // Autorise le frontend a faire des requetes (meme port que react)
 
